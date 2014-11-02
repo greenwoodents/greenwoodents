6e1c45f4b23437024df1577123f5cdc91db628eb
Filip Danisko <f.danisko@gmail.com>
2014-11-02 09:31:14 +0100

    old

--- src/scripts/home/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = angular.module('app.home', []);
-
-require('./HomeCtrl')
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 window.onload = function() {
 	var elements = document.getElementsByClassName('onload');
 	for (var i = elements.length - 1; i >= 0; i--) {
 		elements[i].classList.add("fadeInDown");
 	};
 }
-=======
-angular.module('app', [
-  require('./home').name
-]);
->>>>>>> 36ad3c27f7bc6690decdd2f25c3abbc465148fef
