5088c84e76d5e57cb1a7d8cda22478edfea8a2d7
Filip Danisko <filip@danisko.cz>
2015-06-10 11:15:41 +0200

    loader update, script rewritten, homepage text updated

--- themes/greenwood/source/js/script.js
+++ themes/greenwood/source/js/script.js
@@ -1,17 +1,11 @@
-Pace.once('done', addLoad);
+Pace.once('done', function(){app.afterLoadInitial();});
 
-function addLoad() {
-  document.querySelector('.body-wrap').classList.add('loaded');
-  document.body.classList.add('web-loaded');
+(function(){
+  'use strict';
 
-  defferAll();
-  feedInit();
-}
+  var app = {};
 
-
-
-
-//Helper functions
+  //Helper functions
   var addEvent = function(elem, type, eventHandle) {
     if (elem == null || typeof(elem) == 'undefined') return;
     if ( elem.addEventListener ) {
@@ -29,27 +23,219 @@ function addLoad() {
       el.classList.add('deferload');
     });
   };
-////
 
+  var clearAllMenuState = function() {
+    var body = document.body;
+    body.classList.remove('menu-active');
+    body.classList.remove('menu-active-left');
+    body.classList.remove('menu-from-left-open');
+    body.classList.remove('menu-from-right-open');
+  };
+  ///////
 
-var clearAllMenuState = function() {
-  var body = document.body;
-  body.classList.remove('menu-active');
-  body.classList.remove('menu-active-left');
-  body.classList.remove('menu-from-left-open');
-  body.classList.remove('menu-from-right-open');
-};
+  var feedInit = function(){
+    var feed = document.querySelectorAll('#instafeed');
+    [].forEach.call(feed , function(element, index, array) {
+      var feed = new Instafeed({
+        get: 'user',
+        clientId: '1fbd28fc3ac2446190809988d7bdbab7',
+        userId: 1650642267,
+        accessToken: '1650642267.467ede5.f7b1d8f9e1f4423b910d327f28f7b517',
+        resolution: 'standard_resolution',
+        limit: 7,
+        template: '<img src="{{image}}" />'
+      });
+      feed.run();
+    });
+  };
 
-//Components
-  function components(data) {
-    if (data) {
-      var page = data[0].getAttribute('page') || "Home";
-      document.querySelector('.menu-close').innerHTML = page;
+  //Menu resize
+  var menuSize = function() {
+    var lMq = window.matchMedia( "(min-width: 1300px)" );
+    var mMq = window.matchMedia( "(min-width: 960px)" );
+    var sMq = window.matchMedia( "(min-width: 680px)" );
+    var ssMq = window.matchMedia( "(max-width: 680px)" );
+
+    var space = 10;
+    var boxesOnRow = 4;
+
+    if (sMq.matches) {
+      boxesOnRow = 2;
+    }
+    if (mMq.matches) {
+      boxesOnRow = 3;
+    }
+    if (lMq.matches) {
+      boxesOnRow = 4;
+    }
+    if (ssMq.matches) {
+      boxesOnRow = 1;
     }
 
-    //post load
-    if (document.querySelector('.body-wrap').classList.contains('loaded')) {
-      feedInit();
+    var container = document.querySelector('.js-filtr-content');
+    var containerW = container.offsetWidth;
+
+    var viewportWidth;
+    if (document.compatMode === 'BackCompat') {
+        viewportWidth = document.body.clientWidth;
+    } else {
+        viewportWidth = document.documentElement.clientWidth;
+    }
+
+    var boxSize = Math.round(( (containerW - 4) / boxesOnRow) - space * 2);
+
+    var jsMenuSize = document.querySelectorAll('.js-filtr-content li');
+    [].forEach.call(jsMenuSize, function(reference, index, array) {
+      reference.style.width = boxSize  + "px";
+      reference.style.margin = (space) + "px";
+    });
+  };
+  addEvent(window, "resize", menuSize);
+
+  //afterLoad
+  var afterLoadInitial = function() {
+    document.querySelector('.body-wrap').classList.add('loaded');
+    document.querySelector('.loader').classList.add('loaded');
+
+    defferAll();
+    feedInit();
+    components();
+    initial();
+    menuSize();
+  };
+  //make visible
+  app.afterLoadInitial = afterLoadInitial;
+
+  //Initial
+  var initial = function() {
+
+    //menu filtering init.
+    var jsShow = document.querySelectorAll('.js-filtr a');
+    [].forEach.call(jsShow , function(button, index, btnarray) {
+      var refs = document.querySelectorAll('.js-filtr-content li');
+
+      button.addEventListener('click', function(e){
+        e.preventDefault();
+
+        if (this.classList.contains('activated')) {
+          this.classList.remove('activated');
+        } else {
+          var filterTag = this.getAttribute('filter').toLowerCase().trim();
+          this.classList.add('activated');
+        }
+        filtr();
+      });
+
+      function filtr() {
+        var activeButtons = document.querySelectorAll('.activated');
+        var activeFilters = [];
+
+        [].forEach.call(activeButtons , function(btn, index, array) {
+          var att = btn.getAttribute('filter').toLowerCase().trim();
+          activeFilters.push(att);
+        });
+
+        [].forEach.call(refs , function(reference, index, array) {
+          var elementFilters = reference.getAttribute('filter-class').toLowerCase().trim().split(',');
+          reference.classList.add('fltr-hidden');
+          var contains = 0;
+
+          if (activeFilters.length > 0) {
+            [].forEach.call(elementFilters , function(elementFilter, elementIndex, elementArray) {
+
+              [].forEach.call(activeFilters , function(activeFilter, activeindex, activearray) {
+                elementFilter = elementFilter.trim();
+                if (activeFilter == elementFilter) {
+                  contains++;
+                }
+                if (contains === activearray.length) {
+                  reference.classList.remove('fltr-hidden');
+                }
+              });
+            });
+          } else {
+            reference.classList.remove('fltr-hidden');
+          }
+        });
+      }
+    });
+
+    //Bind Menu Buttons to Smoothstate.
+    var projectOverlay = document.querySelectorAll('.project-overlay');
+    [].forEach.call(projectOverlay , function(element, index, array) {
+      element.addEventListener('click', function(e) {
+        e.preventDefault();
+        var content  = $('#main').smoothState().data('smoothState');
+        content.load(this.href);
+      });
+      element.addEventListener('mouseover', function(e) {
+        e.preventDefault();
+        var content  = $('#main').smoothState().data('smoothState');
+        content.fetch(this.href);
+      });
+    });
+
+    //smoothstate init
+    ;(function($) {
+      'use strict';
+
+        var $body    = $('html, body'),
+        content  = $('#main').smoothState({
+            prefetch: true,
+            pageCacheSize: 0,
+            onStart: {
+                duration: 300,
+                render: function (url, $container) {
+                  document.getElementById('main').classList.remove('is-here');
+                  content.toggleAnimationClass('is-exiting');
+                }
+            },
+            onProgress : {
+                duration: 0, // Duration of the animations, if any.
+                render: function (url, $container) {
+                    $body.css('cursor', 'wait');
+                    $body.find('a').css('cursor', 'wait');
+                    document.getElementById('main').classList.add('content-pending');
+                }
+            },
+            onEnd : {
+                duration: 300, // Duration of the animations, if any.
+                render: function (url, $container, $content) {
+                    $body.css('cursor', 'auto');
+                    $body.find('a').css('cursor', 'pointer');
+                    $container.html($content);
+
+                    document.getElementById('main').classList.remove('content-pending');
+
+                    if (document.body.classList.contains('menu-active')) {
+                      var body = document.body;
+                      clearAllMenuState();
+                      document.getElementById('main').classList.add('is-here');
+
+                    } else {
+                      document.getElementById('main').classList.add('is-here');
+                    }
+
+                    window.scrollTo(0,0);
+                    window.app.components($content);
+                  }
+            }
+        }).data('smoothState');
+    })(jQuery);
+
+    var swipe = new SwipeMe(document.body,{
+      accessClasses: {
+        right: 'menu-active'
+      },
+      direction: ['right']
+    });
+  };
+
+  //Components
+  var components = function(data) {
+    if (data) {
+      var page = data[0].getAttribute('page') || "Home";
+      document.querySelector('.menu-page-title').innerHTML = page;
     }
 
     var jsOpenMenu = document.querySelectorAll('.js-open-menu');
@@ -90,7 +276,6 @@ var clearAllMenuState = function() {
       }, 500);
     });
 
-
     var jsDetails = document.querySelectorAll('.js-hidden-box');
     [].forEach.call(jsDetails , function(element, index, array) {
       element.addEventListener('click', function() {
@@ -103,9 +288,7 @@ var clearAllMenuState = function() {
       document.body.classList.add('body-homepage');
     } else {
       var isHere = document.body.classList.contains('body-homepage');
-      if (isHere) {
-        document.body.classList.remove('body-homepage');
-      }
+      if (isHere) {document.body.classList.remove('body-homepage');}
     }
     var loaded = document.querySelector('.body-wrap').classList.contains('loaded');
     if (loaded) {
@@ -115,197 +298,24 @@ var clearAllMenuState = function() {
         element.classList.remove('js-show');
       });
     }
+
+    //init feed
+    feedInit();
   };
-  components();
-////
+  //make visible
+  app.components = components;
+
+
+
+  //make it global.
+  window.app = app;
+
+})(this);
 
 
 
 
-//Menu
-  var jsShow = document.querySelectorAll('.js-filtr a');
-  [].forEach.call(jsShow , function(button, index, btnarray) {
-    var refs = document.querySelectorAll('.js-filtr-content li');
 
-    button.addEventListener('click', function(e){
-      e.preventDefault();
-
-      if (this.classList.contains('activated')) {
-        this.classList.remove('activated');
-      } else {
-        var filterTag = this.getAttribute('filter').toLowerCase().trim();
-        this.classList.add('activated');
-      }
-      filtr();
-    });
-
-    function filtr() {
-      var activeButtons = document.querySelectorAll('.activated');
-      var activeFilters = [];
-
-      [].forEach.call(activeButtons , function(btn, index, array) {
-        var att = btn.getAttribute('filter').toLowerCase().trim();
-        activeFilters.push(att);
-      });
-
-      [].forEach.call(refs , function(reference, index, array) {
-        var elementFilters = reference.getAttribute('filter-class').toLowerCase().trim().split(',');
-        reference.classList.add('fltr-hidden');
-        var contains = 0;
-
-        if (activeFilters.length > 0) {
-          [].forEach.call(elementFilters , function(elementFilter, elementIndex, elementArray) {
-
-            [].forEach.call(activeFilters , function(activeFilter, activeindex, activearray) {
-              elementFilter = elementFilter.trim();
-              if (activeFilter == elementFilter) {
-                contains++;
-              }
-              if (contains === activearray.length) {
-                reference.classList.remove('fltr-hidden');
-              }
-            });
-          });
-        } else {
-          reference.classList.remove('fltr-hidden');
-        }
-      });
-    }
-  });
-  //Menu size
-  addEvent(window, "resize", menuSize);
-  function menuSize() {
-    var lMq = window.matchMedia( "(min-width: 1300px)" );
-    var mMq = window.matchMedia( "(min-width: 960px)" );
-    var sMq = window.matchMedia( "(min-width: 680px)" );
-    var ssMq = window.matchMedia( "(max-width: 680px)" );
-
-    var space = 10;
-    var boxesOnRow = 4;
-
-    if (sMq.matches) {
-      boxesOnRow = 2;
-    }
-    if (mMq.matches) {
-      boxesOnRow = 3;
-    }
-    if (lMq.matches) {
-      boxesOnRow = 4;
-    }
-    if (ssMq.matches) {
-      boxesOnRow = 1;
-    }
-
-    var container = document.querySelector('.js-filtr-content');
-    var containerW = container.offsetWidth;
-
-    var viewportWidth;
-    if (document.compatMode === 'BackCompat') {
-        viewportWidth = document.body.clientWidth;
-    } else {
-        viewportWidth = document.documentElement.clientWidth;
-    }
-    console.log("vp", viewportWidth);
-
-    var boxSize = Math.round(( (containerW - 4) / boxesOnRow) - space * 2);
-    console.log(boxSize * 4, viewportWidth);
-    var jsMenuSize = document.querySelectorAll('.js-filtr-content li');
-    [].forEach.call(jsMenuSize, function(reference, index, array) {
-      reference.style.width = boxSize  + "px";
-      reference.style.margin = (space) + "px";
-    });
-  }menuSize();
-////
-
-
-//Bind Menu Buttons to Smoothstate.
-var projectOverlay = document.querySelectorAll('.project-overlay');
-[].forEach.call(projectOverlay , function(element, index, array) {
-  element.addEventListener('click', function(e) {
-    e.preventDefault();
-    var content  = $('#main').smoothState().data('smoothState');
-    content.load(this.href);
-  });
-  element.addEventListener('mouseover', function(e) {
-    e.preventDefault();
-    var content  = $('#main').smoothState().data('smoothState');
-    content.fetch(this.href);
-  });
-});
-//smoothstate init
-;(function($) {
-  'use strict';
-
-    var $body    = $('html, body'),
-    content  = $('#main').smoothState({
-        prefetch: true,
-        pageCacheSize: 0,
-        onStart: {
-            duration: 300,
-            render: function (url, $container) {
-              document.getElementById('main').classList.remove('is-here');
-              content.toggleAnimationClass('is-exiting');
-            }
-        },
-        onProgress : {
-            duration: 0, // Duration of the animations, if any.
-            render: function (url, $container) {
-                $body.css('cursor', 'wait');
-                $body.find('a').css('cursor', 'wait');
-                document.getElementById('main').classList.add('content-pending');
-            }
-        },
-        onEnd : {
-            duration: 300, // Duration of the animations, if any.
-            render: function (url, $container, $content) {
-                $body.css('cursor', 'auto');
-                $body.find('a').css('cursor', 'pointer');
-                $container.html($content);
-
-                document.getElementById('main').classList.remove('content-pending');
-
-                if (document.body.classList.contains('menu-active')) {
-                  var body = document.body;
-                  clearAllMenuState();
-                  document.getElementById('main').classList.add('is-here');
-
-                } else {
-                  document.getElementById('main').classList.add('is-here');
-                }
-
-                window.scrollTo(0,0);
-                window.components($content);
-
-
-              }
-        }
-    }).data('smoothState');
-})(jQuery);
-////
-
-function feedInit(){
-  console.log('FEED');
-  var feed = document.querySelectorAll('#instafeed');
-  [].forEach.call(feed , function(element, index, array) {
-    var feed = new Instafeed({
-      get: 'user',
-      clientId: '1fbd28fc3ac2446190809988d7bdbab7',
-      userId: 1650642267,
-      accessToken: '1650642267.467ede5.f7b1d8f9e1f4423b910d327f28f7b517',
-      resolution: 'standard_resolution',
-      limit: 7,
-      template: '<img src="{{image}}" />'
-    });
-    feed.run();
-  });
-}
-
-var swipe = new SwipeMe(document.body,{
-  accessClasses: {
-    right: 'menu-active'
-  },
-  direction: ['right']
-});
 
 
 
